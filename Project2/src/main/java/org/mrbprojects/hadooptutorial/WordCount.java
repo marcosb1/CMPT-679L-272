82834e942471e54748a7c7574af5c7679ea41461
marcosb1 <Marcos.Barbieri1@marist.edu>
2017-10-16 00:48:26 -0400

    Adding correct dependency

--- Project2/src/main/java/org/mrbprojects/hadooptutorial/WordCount.java
+++ Project2/src/main/java/org/mrbprojects/hadooptutorial/WordCount.java
@@ -1,3 +1,5 @@
+package org.mrbprojects.hadooptutorial
+
 import java.io.IOException;
 import java.util.StringTokenizer;
 
--- Project2/src/main/java/org/mrbprojects/hadooptutorial/WordCount2.java
+++ Project2/src/main/java/org/mrbprojects/hadooptutorial/WordCount2.java
@@ -1,3 +1,5 @@
+package org.mrbprojects.hadooptutorial
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
